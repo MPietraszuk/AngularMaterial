67eef453eb33ec433397b4b0ad0bd5d43180dfd0
mpietraszuk <GitHub@MarkZDesign.com>
2019-09-05 13:02:39 -0700

    Layout demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material.module';
 import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: 'demo', loadChildren: './demo/demo.module#DemoModule' },
+  { path: '**', redirectTo: 'demo' }
+];
 
 @NgModule({
   declarations: [
@@ -13,6 +20,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
     MaterialModule,
     FormsModule
   ],
