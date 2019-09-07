a9dc115601234fe7b93f3194f4709b582345ff05
mpietraszuk <GitHub@MarkZDesign.com>
2019-09-07 06:57:08 -0700

    setup fetching data, wired and pulling data

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, NgZone } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
 
 const SMALL_WIDTH_BREAKPOINT = 720;
 
@@ -12,12 +15,21 @@ export class SidenavComponent implements OnInit {
   private mediaMatcher: MediaQueryList =
     matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
 
+  users: Observable<User[]>;
+
   constructor(
-    zone: NgZone) {
+    zone: NgZone,
+    private userService: UserService) {
     zone.run(() => this.mediaMatcher = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`));
   }
 
   ngOnInit() {
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+    this.users.subscribe(data => {
+      console.log(data);
+    });
   }
 
   isScreenSmall(): boolean {
