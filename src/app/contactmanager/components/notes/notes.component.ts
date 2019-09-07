678b9032d47f3f10f472b14f6fc28a12c6b05bf8
mpietraszuk <GitHub@MarkZDesign.com>
2019-09-07 12:59:30 -0700

    wired Mat Table and it's running

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Note } from '../../models/note';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-notes',
@@ -10,9 +11,13 @@ export class NotesComponent implements OnInit {
 
   @Input() notes: Note[];
 
+  displayedColumns = ['position', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
+
   constructor() { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
 }
